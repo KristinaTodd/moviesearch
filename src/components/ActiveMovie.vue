261ba76579a067a16d5ac42cfdd99a293f033d93
Kristina Todd <Kristina.Todd22@gmail.com>
2020-02-26 13:27:23 -0700

    active movie functional

--- src/components/ActiveMovie.vue
+++ src/components/ActiveMovie.vue
@@ -1,21 +1,16 @@
 <template>
-  <div class="row" v-if="details.title">
-    <div class="col-10 text-center card bg-dark text-white">
-      <img src="https://image.tmdb.org/t/p/w500details.poster_path" />
+  <div class="row justify-content-center" v-if="details.title">
+    <div class="col-10 text-center card bg-dark text-white m-1">
+      <img :src='"https://image.tmdb.org/t/p/w500/"+details.poster_path' class="m-1" />
+      <h4>{{ details.title }}</h4>
+      <p>{{details.overview}}</p>
+      <p>Runtime: <span>{{details.runtime}}</span>minutes</p>
+      <span>Release Date: {{details.release_date}}</span>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "ActiveMovie",
-    computed: {
-      details() {
-        return this.$store.state.activeMovie;
-      }
-    },
-    methods: {
-
-    }
-  }
-</script>
+    name: " ActiveMovie", computed: { details() { return this.$store.state.activeMovie; } }, methods: {}
+  } </script>
